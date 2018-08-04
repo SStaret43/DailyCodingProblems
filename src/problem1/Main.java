a06521adc75666f44d198f774add30a59fb64610
Shane Staret <shanestaret@gmail.com>
2018-08-04 01:17:13 -0400

    Added Questions to Each

--- src/problem1/Main.java
+++ src/problem1/Main.java
@@ -1,4 +1,8 @@
-//Using Java
+/*Using Java
+The Problem:
+Given an array of numbers, return whether any two sums to K.
+For example, given [10, 15, 3, 7] and K of 17, return true since 10 + 7 is 17.
+*/
 package problem1;
 
 public class Main {
